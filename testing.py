57313a96059186e4fe22f94e32c4b9942a78cb55
joserigel <joserigel18@gmail.com>
2023-05-06 20:35:54 +0200

    Update

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 import glob
-from teeth import detect_side
+from side_smudge import detect_side
 import cv2 as cv
 
 input_folder_path = './PrePro/input'
@@ -11,7 +11,7 @@ for filename in glob.glob(input_folder_path + '/*.tif'):
     img = detect_side(False, img)
     
     
-    cv.imwrite(output_folder_path + "/" + str(i) + ".png", img)
+    #cv.imwrite(output_folder_path + "/" + str(i) + ".png", img)
     cv.imshow("test", img)
     cv.waitKey(10)
     i += 1
