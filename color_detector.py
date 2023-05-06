281a5de2da40bbe6e07bb102b0ae8184859ed74c
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2023-05-06 22:43:42 +0200

    changes to the greens detector

--- color_detector.py
+++ color_detector.py
@@ -5,20 +5,21 @@ import matplotlib as plt
 import cv2 as cv
 
 
+
+
+"""
+checks for the green makings in the heatmaps; which signalize that a priting error was detected. 
+helper method to find out wether the generated heatmap contains an error signal or not. 
+"""
 def detect_error(filename):
-    # Open the PNG image
     image = Image.open(filename)
-    # Convert the image to RGB mode (if it's not already in RGB mode)
     image = image.convert("RGB")
-    #cv.drawContours(img, contours, -1, (0, 255, 0), 2)
-    # Get the colors in the image
     ans  = set(image.getcolors(image.size[0] * image.size[1]))
-    lower_bound = 70
-    upper_bound = 255
-    count = 0 
-    #print(ans)
+    lower_bound = 70 #ideally lower than 70
+    upper_bound = 255 #keep it a 255 to be sure
+    gray_threshold = 40 #keep it lower than 50
     for i in ans:
-        if i[1][0]<40 and i[1][2] < 40 and i[1][1]>lower_bound and i[1][1]<=upper_bound:
+        if i[1][0]<gray_threshold and i[1][2] < gray_threshold and i[1][1]>lower_bound and i[1][1]<=upper_bound:
             count+=1
 
     if  count>1 :
