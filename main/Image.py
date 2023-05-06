7629d8a0aaa969991350da2aa94eb2036409de60
Retaehc-pop <papop2003@gmail.com>
2023-05-06 15:58:38 +0200

    create combination function and add detect_side_tb

--- main/Image.py
+++ main/Image.py
@@ -12,14 +12,14 @@ class Image:
         self.filename = filename
         self.image_8_bit = self.load_image(is_8bit=True)
         self.image_12_bit = self.load_image()
-        self.crop_image_8_bit,top_left = self.crop(self.image_8_bit)
+        self.crop_image_8_bit, top_left = self.crop(self.image_8_bit)
         self.crop_image_12_bit = self.crop_grid(top_left)
-        
-        #blackspot
+
+        # blackspot
         res = self.blackspot_detect()
         self.blackspot = {
             "right": res[0],
-            "left" : res[1]
+            "left": res[1]
         }
 
     def load_image(self, is_8bit=False) -> Mat:
@@ -28,7 +28,7 @@ class Image:
         """
         if not os.path.isfile(self.filename):
             raise NameError
-        
+
         if is_8bit:
             img = cv.imread(self.filename, cv.IMREAD_GRAYSCALE)
         else:
@@ -40,26 +40,31 @@ class Image:
         """
                 crop to the whole blade
         """
-        template = cv.imread(os.path.join(self.cwd,'asset/cropped_manual.tif'), cv.IMREAD_GRAYSCALE)
+        template = cv.imread(os.path.join(
+            self.cwd, 'asset/cropped_manual.tif'), cv.IMREAD_GRAYSCALE)
         res = cv.matchTemplate(image, template, cv.TM_SQDIFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         top_left = min_loc
-        cropped_image = image[top_left[1]:(top_left[1] + 445), top_left[0]:(top_left[0] + 1338)]
+        cropped_image = image[top_left[1]:(
+            top_left[1] + 445), top_left[0]:(top_left[0] + 1338)]
         return cropped_image, top_left
 
     def crop_grid(self, top_left) -> Mat:
         """
                 Crop Grid from the image
         """
-        cropped_image = self.image_12_bit[top_left[1]:(top_left[1] + 445), top_left[0]:(top_left[0] + 1338)]
-        template_grid = cv.imread(os.path.join(self.cwd,'asset/cropped_grid_area.tif'), cv.IMREAD_GRAYSCALE)
-        bound = cv.matchTemplate(self.crop_image_8_bit, template_grid, cv.TM_SQDIFF_NORMED)
+        cropped_image = self.image_12_bit[top_left[1]:(
+            top_left[1] + 445), top_left[0]:(top_left[0] + 1338)]
+        template_grid = cv.imread(os.path.join(
+            self.cwd, 'asset/cropped_grid_area.tif'), cv.IMREAD_GRAYSCALE)
+        bound = cv.matchTemplate(
+            self.crop_image_8_bit, template_grid, cv.TM_SQDIFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(bound)
         top_left = min_loc
         top_left = (top_left[0] + 10, top_left[1] + 10)
         bottom_right = (top_left[0] + 1160, top_left[1] + 280)
 
-        cropped_image = cropped_image[65:-65,78:-80]
+        cropped_image = cropped_image[65:-65, 78:-80]
 
         return cropped_image
 
@@ -86,20 +91,47 @@ class Image:
                 result[labels == i + 1] = 255
 
         return result
+    
+    def detect_side_tb(self, isBottom: bool):
+        mask = None
+        if (isBottom):
+            mask = cv.imread("left_template.png", cv.IMREAD_UNCHANGED)
+        else:
+            mask = cv.imread("top_template.png", cv.IMREAD_UNCHANGED)
+
+        pic = self.crop_image_8_bit[:32, 78:-77]
+        
+        if isBottom:
+            pic = self.crop_image_8_bit[-32:, 78:-77]
+
+        #blurs template to remove small spots
+        pic = cv.GaussianBlur(pic, (3, 3), 0)
+        #make picture brighter
+        pic = cv.convertScaleAbs(pic, 0.5, 10)
+        pic = cv.cvtColor(pic, cv.COLOR_GRAY2RGBA)
+        #remove blank space with mask 
+        pic = cv.subtract(pic, mask)
+        
+
+        arr = np.where(pic==0, np.nan, pic)
+        mean = np.nanmean(arr)
+        std = np.nanstd(arr)
+
+        return mask
 
     def detect_side_lr(self, isLeft: bool):
         # todo
         mask = None
         if isLeft:
-            mask = cv.imread(os.path.join(self.cwd,"asset/left_template.png"), cv.IMREAD_UNCHANGED)
+            mask = cv.imread(os.path.join(
+                self.cwd, "asset/left_template.png"), cv.IMREAD_UNCHANGED)
         else:
-            mask = cv.imread(os.path.join(self.cwd,"asset/right_template.png"), cv.IMREAD_UNCHANGED)
-
+            mask = cv.imread(os.path.join(
+                self.cwd, "asset/right_template.png"), cv.IMREAD_UNCHANGED)
 
         pic = self.crop_image_8_bit[:, -68:-5]
         if isLeft:
             pic = self.crop_image_8_bit[:, 5:68]
-
 
         # blurs template to remove small spots
         pic = cv.GaussianBlur(pic, (3, 3), 0)
@@ -110,7 +142,7 @@ class Image:
         # remove blank space with mask
         pic = cv.subtract(pic, mask)
 
-        arr = np.where(pic==0, np.nan, pic)
+        arr = np.where(pic == 0, np.nan, pic)
         mean = np.nanmean(arr)
         std = np.nanstd(arr)
 
@@ -119,12 +151,14 @@ class Image:
         mask_u8 = anomalies.astype(np.uint8) * 255
         mask_u8 = cv.cvtColor(mask_u8, cv.COLOR_GRAY2RGB)
         if isLeft:
-            mask = cv.imread(os.path.join(self.cwd,"asset/left_template_nt.png"), cv.IMREAD_COLOR)
+            mask = cv.imread(os.path.join(
+                self.cwd, "asset/left_template_nt.png"), cv.IMREAD_COLOR)
         else:
-            mask = cv.imread(os.path.join(self.cwd,"asset/right_template_nt.png"), cv.IMREAD_COLOR)
+            mask = cv.imread(os.path.join(
+                self.cwd, "asset/right_template_nt.png"), cv.IMREAD_COLOR)
         mask = mask[:445, :63]
         mask = cv.bitwise_not(mask)
-        #remove blankspaces again
+        # remove blankspaces again
         mask_u8 = cv.bitwise_and(mask_u8, mask)
         return mask
 
@@ -149,7 +183,8 @@ class Image:
     def defect_valid(img):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         ret, thresh = cv.threshold(gray, 127, 255, 0)
-        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv.findContours(
+            thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         if len(contours) <= 0:
             return False
         area = np.max(np.array([cv.contourArea(x) for x in contours]))
@@ -161,28 +196,68 @@ class Image:
 
     def blackspot_detect(self):
         """
-        Blackspot detection"""
+            Blackspot detection
+        """
         right_side = self.detect_side_lr(False)
         left_side = self.detect_side_lr(True)
-        
+
         if self.defect_valid(right_side):
             right_side = self.check_area(right_side)
-            
+
         if self.defect_valid(left_side):
             left_side = self.check_area(left_side)
-        return (right_side,left_side)
-            
+        return (right_side, left_side)
+
+    @staticmethod
+    def get_dim(img):
+        return img.shape[:2]
+
+    def combine_image(self):
+        vis = np.zeros((), np.uint8)
+        vertical = cv.vconcat([self.top, self.grid, self.bottom])
+        res = cv.hconcat(
+            [self.blackspot['right'], vertical, self.blackspot['left']])
+        return res
+
+    def combine_image_np(self):
+        dim_left = self.get_dim(self.blackspot['left'])
+        dim_right = self.get_dim(self.blackspot['right'])
+
+        dim_top = self.get_dim(self.blackspot['right'])
+        dim_grid = self.get_dim(self.blackspot['right'])
+        dim_bottom = self.get_dim(self.blackspot['right'])
+
+        # create zeros array of image size
+        vis = np.zeros((
+            max(dim_top[0]+dim_bottom[0]+dim_grid[0],
+                max(dim_left[0], dim_right[0])),
+            dim_left[1]+dim_right[1] +
+            max(dim_grid[1], dim_top[1], dim_bottom[1])
+        ), np.uint8)
+
+        vis[:dim_left[0], :dim_left[1]] = self.blackspot['left']
+
+        vis[:dim_top[0], dim_left[1]:dim_left[1]+dim_top[1]] = self.top
+
+        vis[dim_top[0]:dim_top[0]+dim_grid[0],
+            dim_left[1]:dim_left[1]+dim_grid[1]] = self.grid
+        vis[dim_top[0]+dim_grid[0]:dim_top[0]+dim_grid[0]+dim_bottom[0],
+            dim_left[1]:dim_left[1]+dim_bottom[1]] = self.bottom
         
+        vis[:dim_right[0],dim_left[1]+dim_bottom[1]:] = self.blackspot['right']
+        
+        return vis
+
 
 if __name__ == "__main__":
-    
+
     image_directory = ""
     output_directory = ""
-    test = Image('cam1_0013190202141556102.tif')
-    right, left = test.blackspot_detect()
-    cv.imshow("Left",left)
-    cv.waitKey(0)
-    cv.imshow("Right",right)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    
+    test = Image('test.tif')
+    print(Image.get_dim(test.image_8_bit))
+    # right, left = test.blackspot_detect()
+    # cv.imshow("Left",left)
+    # cv.waitKey(0)
+    # cv.imshow("Right",right)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
