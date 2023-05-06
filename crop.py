91ade8bea54e32d3770c00d77f95b6f7b3803ddd
David Schäfer <david.mcw28@googlemail.com>
2023-05-06 06:45:16 +0200

    DONE FUCKERS

--- crop.py
+++ crop.py
@@ -2,32 +2,35 @@ import cv2 as cv
 import PIL
 import numpy as np
 
-img = cv.imread('cam.tif', cv.IMREAD_GRAYSCALE)
-template = cv.imread('cropped_manual.tif', cv.IMREAD_GRAYSCALE)
+def crop_img(img,img_low_bit):
+
+    template = cv.imread('cropped_manual.tif', cv.IMREAD_GRAYSCALE)
 
 
-res = cv.matchTemplate(img, template, cv.TM_SQDIFF_NORMED)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-top_left = min_loc
-print(top_left)
-# 1338x445 is the dimension of the template
-bottom_right = (top_left[0] + 1338, top_left[1] + 445)
+    res = cv.matchTemplate(img_low_bit, template, cv.TM_SQDIFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    top_left = min_loc
+    print(top_left)
+    # 1338x445 is the dimension of the template
+    bottom_right = (top_left[0] + 1338, top_left[1] + 445)
 
-cropped_image = img[top_left[1]:(top_left[1] + 445), top_left[0]:(top_left[0] + 1338)]
+    cropped_image_low_bit = img_low_bit[top_left[1]:(top_left[1] + 445), top_left[0]:(top_left[0] + 1338)]
+    cropped_image = img[top_left[1]:(top_left[1] + 445), top_left[0]:(top_left[0] + 1338)]
 
 
 
-template_grid = cv.imread('cropped_grid_area.tif', cv.IMREAD_GRAYSCALE)
-bound = cv.matchTemplate(cropped_image, template_grid, cv.TM_SQDIFF_NORMED)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(bound)
+    template_grid = cv.imread('cropped_grid_area.tif', cv.IMREAD_GRAYSCALE)
+    bound = cv.matchTemplate(cropped_image_low_bit, template_grid, cv.TM_SQDIFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(bound)
 
-# 1185x312 dimension of grid area template
-top_left = min_loc
-top_left = (top_left[0] + 10, top_left[1] + 10)
-bottom_right = (top_left[0] + 1160, top_left[1] + 280)
-print(top_left)
-cv.rectangle(cropped_image,top_left, bottom_right, (255, 0, 0), 2)
+    # 1185x312 dimension of grid area template
+    top_left = min_loc
+    top_left = (top_left[0] + 10, top_left[1] + 10)
+    bottom_right = (top_left[0] + 1160, top_left[1] + 280)
+    print(top_left)
+    #cv.rectangle(cropped_image,top_left, bottom_right, (255, 0, 0), 2)
 
-cv.imshow('image', cv.convertScaleAbs(cropped_image)/16)
-cv.rectangle(img,top_left, bottom_right, 255, 2)
-cv.waitKey(0)
+    cropped_image = cropped_image[65:-65,78:-80]
+
+    return cropped_image
+
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from crop import crop_img
+import math
 
 input_folder_path = './PrePro/input'
 output_folder_path = './PrePro/output'
@@ -25,27 +27,31 @@ def remove_small_regions(image,thresh):
             result[labels == i + 1] = 255
 
     return result
-
+numbers=[]
 
 for filename in glob.glob(input_folder_path + '/*.tif'):
 
-    img = cv2.imread(filename,  cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(filename,  cv2.IMREAD_GRAYSCALE | cv2.IMREAD_ANYDEPTH)
+    img_low_bit = cv2.imread(filename,  cv2.IMREAD_GRAYSCALE)
 
-    plt.imshow(img)
-    plt.show()
-
-    img = img[20:-20,40:-40]
-    img = cv2.convertScaleAbs(img*20)
+    img = crop_img(img,img_low_bit)
 
 
+    #img = img[40:-40,80:-80]
 
 
+    kernel_size = 9
+    blur_gray = cv2.GaussianBlur(img , (kernel_size, kernel_size), 0)
 
-    kernel_size = 5
-    blur_gray = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
-    t,blur_gray = cv2.threshold(blur_gray,60,255,cv2.THRESH_BINARY)
+    blur_gray = cv2.adaptiveThreshold(cv2.convertScaleAbs(blur_gray / 16),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,31,2)
 
+
+    contour, hier = cv2.findContours(blur_gray, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contour:
+        cv2.drawContours(blur_gray, [cnt], 0, 255, -1)
 
 
 
@@ -63,18 +69,23 @@ for filename in glob.glob(input_folder_path + '/*.tif'):
     threshold = 20  # minimum number of votes (intersections in Hough grid cell)
     min_line_length = 20  # minimum number of pixels making up a line
     max_line_gap = 20  # maximum gap in pixels between connectable line segments
-    removed_lines = np.copy(blur_gray)   # creating a blank to draw lines on
+    removed_lines = np.copy(blur_gray)  # creating a blank to draw lines on
 
     # Run Hough on edge detected image
     # Output "lines" is an array containing endpoints of detected line segments
-    lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                            min_line_length, max_line_gap)
+    lines = cv2.HoughLinesP(edges,rho = 1,theta = 1*np.pi/180,threshold = 40,minLineLength = 100,maxLineGap = 30)
 
-
+    angle_tol = 5
     for line in lines:
         for x1, y1, x2, y2 in line:
-            cv2.line(removed_lines, (x1, y1), (x2, y2), 0, 1)
+            angle = np.rad2deg(np.arctan2(y2 - y1, x2 - x1))
+            if(abs(abs(angle)-45) < angle_tol):
+                cv2.line(removed_lines, (x1, y1), (x2, y2), 0, 1)
 
+
+
+    #cv2.imshow('image', removed_lines)
+    #cv2.waitKey()
 
     result = remove_small_regions(removed_lines,60)
 
@@ -90,15 +101,42 @@ for filename in glob.glob(input_folder_path + '/*.tif'):
     # Using cv2.erode() method
     line_mask = cv2.erode(line_mask, kernel)
 
+    deadzone_y = 30
+    deadzon_x = 100
 
+    line_mask[0:deadzone_y,:] = 0
+    line_mask[-deadzone_y:-1,:] = 0
+
+
+    line_mask[:,0:deadzon_x] = 0
+    line_mask[:,-deadzon_x:-1] = 0
 
 
     backtorgb = cv2.cvtColor(line_mask, cv2.COLOR_GRAY2RGB)
     backtorgb[:, :, 0] = np.zeros([backtorgb.shape[0], backtorgb.shape[1]])
     backtorgb[:, :, 1] = np.zeros([backtorgb.shape[0], backtorgb.shape[1]])
 
-    added_image = cv2.addWeighted(cv2.cvtColor(img,cv2.COLOR_GRAY2RGB), 0.8, backtorgb, 0.3, 0)
+    added_image = cv2.addWeighted(cv2.cvtColor(cv2.convertScaleAbs(img /16),cv2.COLOR_GRAY2RGB), 0.8, backtorgb, 0.3, 0)
 
-    plt.imshow(added_image)
-    plt.show()
 
+
+    line_mask_red = line_mask / 255
+    residual = np.multiply(line_mask_red,img)
+
+    maske_arr = np.invert( np.array(line_mask,dtype=bool) )
+    mx = np.ma.masked_array(img, mask=maske_arr)
+
+    data = mx[mx.mask == False]
+
+    numbers.append(data)
+
+
+
+    #cv2.imshow('image', cv2.convertScaleAbs(residual /16))
+    #cv2.waitKey(100)
+
+
+plt.hist(np.concatenate(numbers).ravel(), density=False, bins=4095)  # density=False would make counts
+plt.ylabel('Probability')
+plt.xlabel('Data');
+plt.show()
