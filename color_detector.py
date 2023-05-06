c6db8fdb9c68d94eda2c7bdde4af18be0414061c
BenjaminOyarzun17 <benjamin.oyarzun.ma@gmail.com>
2023-05-06 22:52:33 +0200

    added error detection function

--- color_detector.py
+++ color_detector.py
@@ -8,26 +8,23 @@ import cv2 as cv
 
 
 """
-checks for the green makings in the heatmaps; which signalize that a priting error was detected. 
-helper method to find out wether the generated heatmap contains an error signal or not. 
+detects white makings that represent errors in the printing.  
 """
 def detect_error(filename):
     image = Image.open(filename)
     image = image.convert("RGB")
-    ans  = set(image.getcolors(image.size[0] * image.size[1]))
-    lower_bound = 70 #ideally lower than 70
-    upper_bound = 255 #keep it a 255 to be sure
-    gray_threshold = 40 #keep it lower than 50
-    for i in ans:
-        if i[1][0]<gray_threshold and i[1][2] < gray_threshold and i[1][1]>lower_bound and i[1][1]<=upper_bound:
+    colors  = set(image.getcolors(image.size[0] * image.size[1]))
+    threshold= 230
+    count = 0
+    for i in colors:
+        if i[1][0]  >threshold and i[1][2] >threshold and i[1][1]>threshold:
             count+=1
-
-    if  count>1 :
+    if  count>0 : #if there is 
             return True
     return False
 
 
-files  = ["protector_left_green.png","black.png", "grid_greens.png", "grid_black.png"]
+files  = ["blacktest2.png", "black.png"]
 
 for f in files: 
     print(detect_error(f))
