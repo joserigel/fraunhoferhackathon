a35e59ba5350818bc37229a8bc10c7f085e62c6d
Retaehc-pop <papop2003@gmail.com>
2023-05-07 07:09:31 +0200

    data update

--- Image.py
+++ Image.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 from scipy import ndimage,datasets
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks, peak_widths
+from scipy.interpolate import splrep, BSpline
 
 Mat = np.ndarray[int, np.dtype[np.generic]]
 
@@ -33,7 +34,9 @@ class Image:
             "bottom": np.sum(teeth[1],axis=2)
         }
         
-        self.grid = self.grid_detect()
+        mask,img = self.grid_detect()
+        self.grid_silver = self.grid_silver(mask,img)
+        self.grid_black = self.grid_black(mask,img)
         self.processed_image = self.combine_image()
 
     def load_image(self, is_8bit=False) -> Mat:
@@ -162,7 +165,9 @@ class Image:
         # blurs template to remove small spots
         pic = cv.GaussianBlur(pic, (3, 3), 0)
         # make picture brighter
+        cv.imwrite('before.png',pic)
         pic = cv.convertScaleAbs(pic, 0.5, 10)
+        cv.imwrite('after.png',pic)
         pic = cv.cvtColor(pic, cv.COLOR_GRAY2RGBA)
         mask = mask[:445, :63]
         # remove blank space with mask
@@ -205,6 +210,14 @@ class Image:
         else:
             return np.empty_like(img)
         
+    def draw_line_topl_br(self,a,img,shape,w=1):
+        for i in a:
+            cv.line(img, (0, i), (shape[0], i), 255, w)
+
+    def draw_line_bl_topr(self,a,img,shape,w=1):
+        for i in a:
+            cv.line(img,  ( i,shape[1]), (i,0) , 255, w)
+        
     def scan_line(self,blured,image):
         full_img_45_blurred = ndimage.rotate(blured, 45, reshape=True)
         full_img_45 = ndimage.rotate(image, 45, reshape=True)*0
@@ -227,34 +240,172 @@ class Image:
         full_img_45_done_thresh = cv.convertScaleAbs(full_img_45_done_thresh)
         return full_img_45_done_thresh
     
+    def scan_line2(self,blurred,image):
+        res = []
+        
+        tolerance = 1
+        base = 45
+        tries = 11
+        
+        for i in np.linspace(base - tolerance, base+tolerance,tries):
+            full_img_45_blurred = ndimage.rotate(blurred, i, reshape=True)
+            
+            y_sum = np.sum(full_img_45_blurred,0)
+            x_sum = np.sum(full_img_45_blurred,1)
+            
+            valleys_y, _ = find_peaks(-y_sum, distance=10, prominence=50000)
+            valleys_x, _ = find_peaks(-x_sum, distance=10, prominence=50000)
+
+            widthsx, h_eval, left_ips, right_ips = peak_widths(-x_sum, valleys_x, rel_height=0.5)
+            widthsy, h_eval, left_ips, right_ips = peak_widths(-y_sum, valleys_y, rel_height=0.5)
+            res.append((np.sum(np.concatenate((widthsx,widthsy), axis=None)),i))
+        smallest = min(res, key=lambda x: x[0])
+        best_angle = smallest[1]
+        ##############################################
+        full_img_45_blurred = ndimage.rotate(blurred, best_angle, reshape=True)
+        y_sum = np.sum(full_img_45_blurred,0)
+        x_sum = np.sum(full_img_45_blurred,1)
+        valleys_x, _ = find_peaks(-x_sum, distance=10, prominence=50000)
+        valleys_y, _ = find_peaks(-y_sum, distance=10, prominence=50000)
+        corr_fac_x = splrep(valleys_x,x_sum[valleys_x], s=int(len(valleys_x)/2))
+        corr_fac_y = splrep(valleys_y,y_sum[valleys_y], s=int(len(valleys_y)/2))
+        xp = np.linspace(0, len(x_sum), len(x_sum))
+        yp = np.linspace(0, len(y_sum), len(y_sum))
+        c_x = BSpline(*corr_fac_x)(xp)
+        c_y = BSpline(*corr_fac_y)(yp)
+        x_sum_corr = x_sum-c_x
+        y_sum_corr = y_sum-c_y
+        ########################################################
+        full_img_45_blurred = ndimage.rotate(blurred, best_angle, reshape=True)
+        full_img_45 = ndimage.rotate(image, best_angle, reshape=True)*0
+        image_s = full_img_45_blurred.shape
+        y_sum = y_sum_corr
+        x_sum = x_sum_corr
+        valleys_x, _ = find_peaks(-x_sum, distance=10, prominence=20000)
+        valleys_y, _ = find_peaks(-y_sum, distance=10, prominence=20000)
+        valleys_x = valleys_x[1:-1]
+        valleys_y = valleys_y[1:-1]
+        a = np.extract((np.logical_and(valleys_x > 0, valleys_x <= 75)), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 7)
+        a = np.extract((np.logical_and(valleys_x > 75, valleys_x <= 148)), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 6)
+        a = np.extract((np.logical_and(valleys_x > 148, valleys_x <= 210)), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 4)
+        a = np.extract((np.logical_and(valleys_x > 210, valleys_x <= 240)), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 2)
+        a = np.extract(np.logical_and(valleys_x > 240, valleys_x <= 808), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 1)
+        a = np.extract(np.logical_and(valleys_x > 808, valleys_x <= 840), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 2)
+        a = np.extract(np.logical_and(valleys_x > 840, valleys_x <= 870), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 4)
+        a = np.extract(np.logical_and(valleys_x > 870, valleys_x <= 960), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 6)
+        a = np.extract(np.logical_and(valleys_x > 960, valleys_x <= 1050), valleys_x)
+        self.draw_line_topl_br(a, full_img_45, image_s, 7)
+
+        ##### YAbschnitte
+
+        a = np.extract((np.logical_and(valleys_y > 0, valleys_y <= 75+20)), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 7)
+        a = np.extract((np.logical_and(valleys_y > 75, valleys_y <= 148)), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 6)
+        a = np.extract((np.logical_and(valleys_y > 148, valleys_y <= 178)), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 4)
+        a = np.extract((np.logical_and(valleys_y > 178, valleys_y <= 195)), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 2)
+        a = np.extract(np.logical_and(valleys_y > 195, valleys_y <= 808), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 1)
+        a = np.extract(np.logical_and(valleys_y > 808, valleys_y <= 840), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 2)
+        a = np.extract(np.logical_and(valleys_y > 840, valleys_y <= 870), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 4)
+        a = np.extract(np.logical_and(valleys_y > 870, valleys_y <= 960), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 6)
+        a = np.extract(np.logical_and(valleys_y > 960, valleys_y <= 1050), valleys_y)
+        self.draw_line_bl_topr(a, full_img_45, image_s, 7)
+        full_img_45_done = ndimage.rotate(full_img_45, -best_angle, reshape=True)[580:874+1,148:1306+2]
+        ret, full_img_45_done_thresh = cv.threshold(full_img_45_done, 127, 4095, cv.THRESH_BINARY)
+        full_img_45_done_thresh = cv.convertScaleAbs(full_img_45_done_thresh)
+        return full_img_45_done_thresh
     
+    def grid_silver(self,mask,img):
+        
+        mask =  np.array(mask, dtype=np.uint8)
+        kernel = np.ones((3, 3), np.uint8)
+        mask_dil = cv.dilate(mask, kernel, iterations=1)
+        mask_dil = np.invert(mask_dil)/255
+        
+        mask_h, mask_w = mask_dil.shape
+         
+         #Top Left
+        triangle_cnt = np.array([(0,0), (100,0), (0,100)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
+
+        # Top Right
+        triangle_cnt = np.array([(mask_w, 0), (mask_w-100, 0), (mask_w, 100)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
+
+        # Bot. Left
+        triangle_cnt = np.array([(0, mask_h), (100, mask_h), (0, mask_h - 100)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
+
+        # Bot. Right
+        triangle_cnt = np.array([(mask_w, mask_h), (mask_w - 100, mask_h), (mask_w,mask_h- 100)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
+        
+        img_masked = np.multiply(mask_dil,img)
+        cv.imwrite("img_mask.png",img_masked)
+        
+        return mask_dil
+        
+        
     def grid_detect(self):
         img = self.crop_image_12_bit
-        kernel_size = 11
+        
+        kernel_size = 13
         blur_gray = cv.GaussianBlur(img , (kernel_size, kernel_size), 0)
+        img = img[10:-10,10:-10]
+        blur_gray = blur_gray[10:-10,10:-10]
+        line_mask = self.scan_line2(blur_gray,img)
+        cv.imwrite('mask.png',line_mask)
+
+        return line_mask, img
+    
+    
+    def grid_black(self,mask_dil,img):
+        mask_h, mask_w = mask_dil.shape
+        #Top Left
+        triangle_cnt = np.array([(0,0), (125,0), (0,125)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
+
+        # Top Right
+        triangle_cnt = np.array([(mask_w, 0), (mask_w-125, 0), (mask_w, 125)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
+
+        # Bot. Left
+        triangle_cnt = np.array([(0, mask_h), (125, mask_h), (0, mask_h - 125)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
+
+        # Bot. Right
+        triangle_cnt = np.array([(mask_w, mask_h), (mask_w - 125, mask_h), (mask_w,mask_h- 125)])
+        cv.drawContours(mask_dil, [triangle_cnt], 0, 0, -1)
         
-        line_mask = self.scan_line(blur_gray,img)
-        
-        img = img[40:-40,80:-80]
-        line_mask= line_mask[40:-40,80:-80]
-        img_median_blurred = img
-        
-        maske_arr = np.array(line_mask, dtype=bool)
-        mx_only_squares = np.ma.masked_array(img_median_blurred, mask=maske_arr)
+        maske_arr = np.array(mask_dil, dtype=bool)
+        mx_only_squares = np.ma.masked_array(img, mask=maske_arr)
         data = mx_only_squares[mx_only_squares.mask == False]
         
         std_dev = np.std(data)
         av = np.average(data)
-        
-        maske_arr = np.invert(np.array(line_mask, dtype=bool))
-        mx = np.ma.masked_array(img_median_blurred, mask=maske_arr)
+
+        maske_arr = np.invert(np.array(mask_dil, dtype=bool))
+        mx = np.ma.masked_array(img, mask=maske_arr)
         data = mx[mx.mask == False]
-        
-        self.grid_value = np.count_nonzero(data > av-0.5*std_dev)
 
-        maske_arr = np.array(line_mask, dtype=bool)
+        res = np.count_nonzero(data > av-0.5*std_dev)
+
+        maske_arr = np.array(mask_dil, dtype=bool)
         raw = np.multiply(maske_arr, img)
-
         heat = np.array( (raw > av-0.5*std_dev)*255, dtype=np.uint8)
 
         kernel = np.ones((3, 3), np.uint8)
@@ -262,7 +413,7 @@ class Image:
         img_dilation_border = cv.dilate(heat, kernel, iterations=3)
 
         img_dilation = np.subtract(img_dilation_border,img_dilation)
-
+        
         return img_dilation
     
     @staticmethod
@@ -331,7 +482,7 @@ class Image:
         dim_right = self.get_dim(self.blackspot['right'])
 
         dim_top = self.get_dim(self.teeth['top'])
-        dim_grid = self.get_dim(self.grid)
+        dim_grid = self.get_dim(self.grid_black)
         dim_bottom = self.get_dim(self.teeth['bottom'])
 
         array_size = (
@@ -351,7 +502,7 @@ class Image:
         vis[:dim_top[0], dim_left[1]:dim_left[1]+dim_top[1]] = self.teeth['top']
 
         vis[dim_top[0]+offset[0]:dim_top[0]+offset[0]+dim_grid[0],
-            dim_left[1]+offset[1]:dim_left[1]+offset[1]+dim_grid[1]] = self.grid
+            dim_left[1]+offset[1]:dim_left[1]+offset[1]+dim_grid[1]] = self.grid_black
         
         vis[dim_top[0]+offset[0]+dim_grid[0]+offset[0]:dim_top[0]+offset[0]+dim_grid[0]+offset[0]+dim_bottom[0],
             dim_left[1]:dim_left[1]+dim_bottom[1]] = self.teeth['bottom']
@@ -365,7 +516,12 @@ if __name__ == "__main__":
 
     image_directory = ""
     output_directory = ""
-    test = Image('test\\teeth2.tif')
-    cv.imshow("",test.processed_image)
+    test = Image('test.tif')
+    print(test.get_dim(test.grid_silver))
+    cv.imshow("",test.grid_silver)
+    cv.imshow("s",test.grid_black)
+    print(test.get_dim(test.grid_black))
+    res = np.add(test.grid_black,test.grid_silver)
+    cv.imshow('res',res)
     cv.waitKey(0)
     cv.destroyAllWindow()
